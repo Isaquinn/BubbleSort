d73edf70d7a36cf69d5c8109c61cfae1691b5921
Isaque de Araujo <isaquearaujo22@gmail.com>
2016-03-16 22:01:53 -0300

    Bubble Sort
    Só falta fazer o efeito louco no gráfico.

---
+++ BubbleSort2.0/BubbleSort2.0/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace BubbleSort2._0
+{
+    partial class sort
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.Inicializar = new System.Windows.Forms.Button();
+            this.Adicionar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.Zerar = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Tempo = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Inicializar
+            // 
+            this.Inicializar.Location = new System.Drawing.Point(12, 40);
+            this.Inicializar.Name = "Inicializar";
+            this.Inicializar.Size = new System.Drawing.Size(63, 23);
+            this.Inicializar.TabIndex = 0;
+            this.Inicializar.Text = "Inicializar";
+            this.Inicializar.UseVisualStyleBackColor = true;
+            this.Inicializar.Click += new System.EventHandler(this.Inicializar_Click);
+            // 
+            // Adicionar
+            // 
+            this.Adicionar.Location = new System.Drawing.Point(150, 40);
+            this.Adicionar.Name = "Adicionar";
+            this.Adicionar.Size = new System.Drawing.Size(75, 23);
+            this.Adicionar.TabIndex = 1;
+            this.Adicionar.Text = "Adicionar";
+            this.Adicionar.UseVisualStyleBackColor = true;
+            this.Adicionar.Click += new System.EventHandler(this.Adicionar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "MinValor";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(122, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "MaxValor";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(54, 10);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(46, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(179, 10);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(46, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(3, 130);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(222, 225);
+            this.listBox1.TabIndex = 7;
+            // 
+            // Zerar
+            // 
+            this.Zerar.Location = new System.Drawing.Point(2, 93);
+            this.Zerar.Name = "Zerar";
+            this.Zerar.Size = new System.Drawing.Size(103, 23);
+            this.Zerar.TabIndex = 8;
+            this.Zerar.Text = "Zerar";
+            this.Zerar.UseVisualStyleBackColor = true;
+            this.Zerar.Click += new System.EventHandler(this.Zerar_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 367);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Tempo de execução";
+            // 
+            // Tempo
+            // 
+            this.Tempo.AutoSize = true;
+            this.Tempo.Location = new System.Drawing.Point(160, 358);
+            this.Tempo.Name = "Tempo";
+            this.Tempo.Size = new System.Drawing.Size(0, 13);
+            this.Tempo.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(131, 98);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "NºItens";
+            
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(189, 98);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "0";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(70, 64);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(90, 23);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "BubbleSort";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(233, 367);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(586, 368);
+            this.listBox2.TabIndex = 17;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(233, 10);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "numbersgrafics";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(599, 351);
+            this.chart1.TabIndex = 18;
+            this.chart1.Text = "chart1";
+           
+            // 
+            // sort
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(844, 395);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Tempo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Zerar);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Adicionar);
+            this.Controls.Add(this.Inicializar);
+            this.Name = "sort";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Inicializar;
+        private System.Windows.Forms.Button Adicionar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button Zerar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label Tempo;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ BubbleSort2.0/BubbleSort2.0/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace BubbleSort2._0
+{
+
+    public partial class sort : Form
+    {
+        Graphics g;
+        public static List<string> numeros = new List<string>();
+        int valor;
+        Stopwatch stopwatch = new Stopwatch();
+        TimeSpan time;
+        public static int contador;
+        Random random = new Random();
+        public sort()
+        {
+            InitializeComponent();
+
+
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+         
+
+            stopwatch.Start();
+            listBox1.Items.Clear();
+
+            BubbleSort(numeros);
+
+            listBox1.Items.AddRange(numeros.ToArray());
+          
+
+            chart1.Series["numbersgrafics"].Points.DataBindY(numeros);
+            chart1.Refresh();
+
+
+
+            stopwatch.Stop();
+            time = stopwatch.Elapsed;
+            Tempo.Text = time.ToString();
+        }
+        private void DrawSamples()
+        {
+            g = listBox2.CreateGraphics();
+            g.Clear(Color.White);
+
+            for (int i = 0; i < numeros.Count; i++)
+            {
+                int x = listBox2.Width / numeros.Count * i;
+                //BubbleSort(numeros);
+                Pen pen = new Pen(Color.Black);
+                g.DrawLine(pen, new Point(x, listBox2.Height),
+                new Point(x, (int)(listBox2.Height - Convert.ToInt32((string)numeros[i]))));
+            }
+
+        }
+
+
+        private void Inicializar_Click(object sender, EventArgs e)
+        {
+
+            chart1.Series["numbersgrafics"].Points.Clear();
+            listBox1.Items.Clear();
+            numeros.Clear();
+
+            contador = numeros.Count;
+
+
+
+
+
+            if (textBox1.Text == "" && textBox2.Text == "")
+            {
+                return;
+            }
+            else if (textBox1.Text != "" && textBox2.Text == "")
+            {
+                return;
+            }
+            else if (textBox1.Text == "" && textBox2.Text != "")
+            {
+                return;
+            }
+            else {
+
+
+                while (contador < 20)
+                {
+                    valor = random.Next(Convert.ToInt32(textBox2.Text), Convert.ToInt32(textBox1.Text));
+                    NoRepeat(valor.ToString());
+
+                    contador++;
+
+                    chart1.Series["numbersgrafics"].Points.DataBindY(numeros);
+
+                }
+
+
+                listBox1.Items.AddRange(numeros.ToArray());
+
+
+
+                label5.Text = numeros.Count.ToString();
+            }
+            DrawSamples();
+
+
+        }
+        static void BubbleSort(List<string> numeros)
+        {
+            for (int i = 0; i < numeros.Count; i++)
+            {
+                for (int k = i + 1; k < numeros.Count; k++)
+                {
+                    if (int.Parse(numeros[i]) > int.Parse(numeros[k]))
+                    {
+                        int temp = (int.Parse(numeros[i]));
+                        numeros[i] = numeros[k];
+                        numeros[k] = temp.ToString();
+                    }
+                }
+            }
+        }
+
+        static void NoRepeat(string item)
+        {
+
+            if (!numeros.Contains(item))
+            {
+
+                numeros.Add(item);
+            }
+            else
+            {
+                contador--;
+            }
+        }
+
+        private void Adicionar_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            numeros.Clear();
+            contador = numeros.Count;
+            while (contador < 2000)
+            {
+
+                valor = random.Next(Convert.ToInt32(textBox2.Text), Convert.ToInt32(textBox1.Text));
+                contador++;
+                NoRepeat(valor.ToString());
+                chart1.Series["numbersgrafics"].Points.DataBindY(numeros);
+            }
+
+            listBox1.Items.AddRange(numeros.ToArray());
+            label5.Text = numeros.Count.ToString();
+            DrawSamples();
+        }
+
+        private void Zerar_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            numeros.Clear();
+            label5.Text = "";
+        }
+
+
+    }
+}
